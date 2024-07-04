8842099ea50142da1f0c6e1e160dce02c0e6c708
tharun5343 <tharun5343@gmail.com>
2024-07-04 19:14:11 +0530

    server setup

---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+import app from "./app.js";
+import connectDB from "./db/db.js"
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+});
+
+connectDB()
+    .then(
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(
+                `App listening on http://localhost:${process.env.PORT}/`
+            );
+        })
+    )
+    .catch((err) => {
+        console.log("MongoDB connection failed", err);
+    });
