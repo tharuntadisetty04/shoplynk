4f496ea48574f3704222dec2bdba9fecf8e566ad
tharun5343 <tharun5343@gmail.com>
2024-07-05 11:23:10 +0530

    revert testing

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAMEsas}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
         console.log(`MongoDB connected! DB HOST : ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB connection error : ", error);
