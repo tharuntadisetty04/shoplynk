692f0cd0fa7e2537827534351d9c2ec63c085d4d
tharun5343 <tharun5343@gmail.com>
2024-07-08 11:15:25 +0530

    add user routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,34 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.send("Server running ...")
-})
+    res.send("Server running ...");
+});
 
 //Routes
 import productRouter from "./routes/product.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 //product routes
-app.use("/api/v1/products", productRouter)
+app.use("/api/v1/products", productRouter);
+
+//user route
+app.use("/api/v1/user", userRouter);
 
 app.on("error", (err) => {
     console.log("App error : ", err);
---
+++ server/src/routes/user.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+    getCurrentUser,
+    loginUser,
+    logoutUser,
+    registerUser,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+export default router;
