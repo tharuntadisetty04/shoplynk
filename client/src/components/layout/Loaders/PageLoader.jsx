a5d974ab54b4c372ae546a7f3cf6f03b2069faa6
tharun5343 <tharun5343@gmail.com>
2024-09-27 14:27:19 +0530

    made bg transparent

--- client/src/components/layout/Loaders/PageLoader.jsx
+++ client/src/components/layout/Loaders/PageLoader.jsx
@@ -1,6 +1,6 @@
 const PageLoader = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-neutral-100 -mt-8">
+        <div className="flex items-center justify-center min-h-screen bg-neutral-100 -mt-8 bg-transparent">
             <div className="flex flex-col gap-6 w-full items-center justify-center">
                 <div className="w-20 h-20 border-4 border-transparent animate-spin flex items-center justify-center border-t-blue-600 rounded-full">
                     <div className="w-16 h-16 border-4 border-transparent animate-spin border-t-blue-400 rounded-full"></div>
